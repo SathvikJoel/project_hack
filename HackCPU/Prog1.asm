17ea8dbe4f67a75b908836b4ddd6950edbccfdf3
ennasaiteja <70141503+ennasaiteja@users.noreply.github.com>
2020-11-06 18:58:23 +0530

    Add files via upload

---
+++ HackCPU/Prog1.asm
@@ -0,0 +1,16 @@
+
+//    d = a + b - c
+
+
+      @a      // a refers to some mem. location.
+      D=M     // D=a
+      @b      // b refers to some mem. location.
+      D=D+M   // D=a+b
+      @c      // c refers to some mem. location.
+      D=D-M   // D=a+b-c
+      @d      // d refers to some mem. location.
+      M=D     // d=a+b-c
+
+ (END)
+      @END
+      0;JMP   // the infinite loop is a standard way to "terminate" the execution of a Hack program.
